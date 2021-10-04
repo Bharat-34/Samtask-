d3ce99aeaaafd3ab098a41f963c3c11275a8071a
Bharat <bharat.kumar@gopoolit.com>
2021-10-04 08:40:54 +0530

    Initial Commit

---
+++ Samtask/SamtaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  SamtaskApp.swift
+//  Samtask
+//
+//  Created by Tarak Jaan on 04/10/21.
+//
+
+import SwiftUI
+
+@main
+struct SamtaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
